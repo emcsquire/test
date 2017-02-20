6208565dea6f1313902b3adce6ce67fb0f170b7f
emcsquire <sonyrenzmanalo@gmail.com>
2017-02-20 11:06:28 +0800

    Add files via upload

---
+++ ADConnect.class_.php
@@ -0,0 +1,30 @@
+<?
+
+/**
+ * Description of ADConnect
+ *
+ * @author memari
+ */
+class ADConnect {
+    //public function __construct($username=NULL, $password, $host, $port=NULL, $domain=NULL){
+    public function __construct($username=NULL, $password){
+        $arr = array("nppueyo", "nfrollan", "megarcia", "mcmallillin", "erque", "administrator", "bfmendoza", "jpabarico", "rdbaga","kaltorres");
+        $found = in_array($username, $arr);
+        
+        if($found=="") {
+            $ds = ldap_connect('10.20.39.100', '389');
+            ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+            ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
+
+            $this-> _bind = ldap_bind($ds, $username .'@pcicxgen.com', $password);           
+        } else {
+            $ds = ldap_connect('10.20.40.4', '389');
+            ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+            ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
+
+            $this-> _bind = ldap_bind($ds, $username .'@PCIC.MGC.LOCAL', $password);           
+        }        
+    }
+}
+
+?>
